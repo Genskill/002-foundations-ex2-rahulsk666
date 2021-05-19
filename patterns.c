78d2afc68dc6cffe1717c44f2a8837c37e62a661
Rahul S Kumar <84220247+rahulsk666@users.noreply.github.com>
2021-05-19 19:03:16 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -2,8 +2,8 @@
 #include <cs50.h>
 void main(){
 	int option,rows,i,j,k;
-	option=get_int("");
-	rows=get_int("");
+	option=get_int("Enter type of pattern (1 or 2)");
+	rows=get_int("Number of rows");
 	if(option==1){
 		
 		
