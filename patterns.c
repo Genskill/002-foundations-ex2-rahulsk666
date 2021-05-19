fa851d4611ad1f8a7e2d3769211fa0a6382c6310
Rahul S Kumar <84220247+rahulsk666@users.noreply.github.com>
2021-05-19 18:41:21 +0530

    Add files via upload

---
+++ patterns.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include<cs50.h>
+void pattern1(void);
+void pattern2(void);
+int main(void){
+    int pat = get_int("Enter type of pattern (1 or 2) ");
+    int h = get_int("Number of rows ");
+    switch (pat)
+    {
+    case 1:
+        pattern1();
+        break;
+    case 2:
+        pattern2();
+    
+    default:
+        break;
+    }
+}
+void patter1(void){
+     int h = get_int("Number of rows ");
+     for (int i=0;i<=h;i++) 
+     {
+         for ( j = h;j>=o; j--)
+         {
+             printf("#");
+         }
+        printf("\n");   }
+
+void patter2(void){
+     int h = get_int("Number of rows ");
+     for (int i=0;i<=h;i++) 
+     {
+         for (int  j = h-(i+1),k=0; j<0;k++, j--)
+         {
+             printf(" ");
+         }
+        for ( j= k;j,h ; j++){
+            printf("")#
+        }
+    }
+
+
+
+
